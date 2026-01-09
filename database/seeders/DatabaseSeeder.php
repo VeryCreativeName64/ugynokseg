d18d67f155e5a0adcb5fc48e288ab104bd710e8b
VeryCreativeName64 <marekbendeguz@gmail.com>
2026-01-09 14:38:36 +0100

    users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,12 +15,29 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // User::factory(10)->create();
+{
+    User::create([
+        'name' => 'Teszt Elek',
+        'email' => 'elek@example.com',
+        'password' => Hash::make('password'),
+        'vip' => true,
+        'permission' => 'admin'
+    ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    User::create([
+        'name' => 'Kovács János',
+        'email' => 'janos@example.com',
+        'password' => Hash::make('password'),
+        'vip' => false,
+        'permission' => 'user'
+    ]);
+
+    User::create([
+        'name' => 'Nagy Anna',
+        'email' => 'anna@example.com',
+        'password' => Hash::make('password'),
+        'vip' => true,
+        'permission' => 'user'
+    ]);
+}
 }
