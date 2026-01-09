0cb1aecb562a9b1426ddf9529b7c1b4f3c96487d
VeryCreativeName64 <marekbendeguz@gmail.com>
2026-01-09 15:20:26 +0100

    UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function store(Request $request)
+    {
+       
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'vip' => 'boolean',
+            'permission' => 'string'
+        ]);
+
+        $validated['password'] = Hash::make($validated['password']);
+        $user = User::create($validated);
+        
+        return response()->json($user, 201);
+    }
+
+    public function show($id)
+    {
+        return User::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'email' => 'sometimes|string|email|max:255|unique:users,email,'.$id,
+            'vip' => 'boolean',
+            'permission' => 'string'
+        ]);
+
+        $user->update($validated);
+        return response()->json($user, 200);
+    }
+
+    public function destroy($id)
+    {
+        User::findOrFail($id)->delete();
+        return response()->json(['message' => 'Felhasználó törölve'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\ParticipateController;
 use App\Http\Controllers\AgencyController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('participates', ParticipateController::class);
 Route::apiResource('agencies', AgencyController::class);
+Route::apiResource('users', UserController::class);
