b30ffdfb0f14b1db70efdc0d1c27b3bc751d5f6d
VeryCreativeName64 <marekbendeguz@gmail.com>
2026-01-09 15:18:14 +0100

    AgencyController

---
+++ app/Http/Controllers/AgencyController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Agency;
+use Illuminate\Http\Request;
+
+class AgencyController extends Controller
+{
+    public function index()
+    {
+       
+        return Agency::all();
+    }
+
+    public function store(Request $request)
+    {
+       
+        $agency = Agency::create($request->all());
+        return response()->json($agency, 201);
+    }
+
+    public function show($id)
+    {
+        
+        return Agency::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+       
+        $agency = Agency::findOrFail($id);
+        $agency->update($request->all());
+        return response()->json($agency, 200);
+    }
+
+    public function destroy($id)
+    {
+      
+        Agency::findOrFail($id)->delete();
+        return response()->json(['message' => 'Ügynökség sikeresen törölve'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\ParticipateController;
+use App\Http\Controllers\AgencyController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('participates', ParticipateController::class);
+Route::apiResource('agencies', AgencyController::class);
